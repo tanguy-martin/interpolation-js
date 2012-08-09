8f116bde708d979120b078b0613e088deeebe7ed
Tanguy Martin <martin_tanguy@yahoo.fr>
2012-08-09 21:22:17 +0100

    put stuff in correct folders, added html5 input range polyfill but it still does not work in non-webkit browsers :(

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 (function() {
-    var b = document.body,
+    var b = document.body, 
+        whoow = document.getElementById('whoow'),
+        whoowImage = document.getElementById('whoow-image'),
     	animationListener = function(e) {
     		b.style.backgroundColor = 'hsl(' + Math.ceil((Math.random()*360)) + ', 100%, 50%)';
+            whoow.style.color = 'hsl(' + Math.ceil((Math.random()*360)) + ', 100%, 50%)';
     	};
 
     b.addEventListener("oAnimationIteration", animationListener, false);
@@ -11,6 +14,8 @@
     b.addEventListener("animationiteration", animationListener, false);
 
     document.getElementById('changeSpeed').addEventListener('change', function(e) {
+        whoow.style.visibility = e.target.value > 40 ? 'visible' : 'hidden';
+        whoowImage.style.visibility = e.target.value > 40 ? 'visible' : 'hidden';
 		b.style.webkitAnimationDuration = '' + 1/e.target.value + 's';
 		b.style.mozAnimationDuration = '' + 1/e.target.value + 's';
 		b.style.oAnimationDuration = '' + 1/e.target.value + 's';
